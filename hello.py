8edba53efeb58c36cea7cbcc2773c520041902f6
Shambhavi Gupta <52174911+shambhavi2001@users.noreply.github.com>
2021-06-02 21:39:06 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+
+@app.route('/admin')
+def hello_admin():
+    return 'Hello, Admin!'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello, Guest! I assume you are %s :)' %guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name=='admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+if __name__ == '__main__':
+   app.run()
+''' host=0.0.0.0
+port=5000
+debug=false
+options=To be forwarded to underlying Werkzeug server.
+''' 
+
+
+
+
